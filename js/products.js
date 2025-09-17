d27607671eb2a5dcc02b5283ee9e9a9d5ef9ac9a
Donovan Acosta <dnvnacosta81@gmail.com>
2025-09-17 13:24:16 -0300

    update products

--- js/products.js
+++ js/products.js
@@ -15,26 +15,73 @@ getJSONData(`${PRODUCTS_URL}${localStorage.getItem('catID')}${EXT_TYPE}`)
             <div class="alert alert-danger text-center" role="alert">
                 <h4 class="alert-heading">No hay productos disponibles!</h4>
             </div>`;
-        } else {
-            for (const product of products) {
-                containerProducts.innerHTML += `
-                <div class="col-12 col-md-4 col-lg-3 mb-4 productosContainer">
-                    <div class="card h-100">
-                        <img src="${product.image}" class="card-img-top" alt="${product.name} image">
-                        <div class="card-body">
-                            <h5 class="card-title">${product.name}</h5>
-                            <p class="card-text">${product.description}</p>
-                        </div>
-                        <div class="card-footer d-flex justify-content-between">
-                            <span >${product.currency} <span id="price">${product.cost}</span></span>
-                            <span id="soldCount">${product.soldCount}</span>
-                        </div>
-                    </div>
-                </div>
-            `
-            }
+
+            return;
         }
+
+        products.forEach(product => {
+            createCardProduct(product);
+        });
     });
+
+function createCardProduct(product) {
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'col-12 col-md-4 col-lg-3 mb-4 productosContainer';
+
+    const anchor = document.createElement('a');
+    anchor.href = './product-info.html';
+    anchor.className = 'text-decoration-none text-reset';
+
+    anchor.addEventListener('click', () => {
+        localStorage.setItem('productID', product.id);
+    });
+
+    const card = document.createElement('div');
+    card.className = 'card h-100';
+
+    const img = document.createElement('img');
+    img.src = product.image;
+    img.className = 'card-img-top';
+    img.alt = `${product.name} image`;
+
+    const body = document.createElement('div');
+    body.className = 'card-body';
+
+    const title = document.createElement('h5');
+    title.className = 'card-title';
+    title.textContent = product.name;
+
+    const desc = document.createElement('p');
+    desc.className = 'card-text';
+    desc.textContent = product.description;
+
+    body.appendChild(title);
+    body.appendChild(desc);
+
+    const footer = document.createElement('div');
+    footer.className = 'card-footer d-flex justify-content-between';
+
+    const price = document.createElement('p');
+    price.innerHTML = `${product.currency} <span class="price" id="price">${product.cost}</span>`;
+
+    const sold = document.createElement('p');
+    sold.className = 'soldCount';
+    sold.id = 'soldCount'
+    sold.style.display = product.soldCount > 0 ? 'block' : 'none';
+    sold.textContent = product.soldCount;
+
+    footer.appendChild(price);
+    footer.appendChild(sold);
+
+    card.appendChild(img);
+    card.appendChild(body);
+    card.appendChild(footer);
+    anchor.appendChild(card);
+    cardContainer.appendChild(anchor);
+    containerProducts.appendChild(cardContainer);
+
+
+}
 
 const productosContainer = containerProducts.getElementsByClassName('productosContainer'); // Obtengo todas las cartas de productos
 
@@ -98,8 +145,8 @@ function ordenarProductos() {
     }
     else if (tipo === "relevanciaDesc") {
         Array.from(productosContainer).sort((a, b) => {
-            const price = Number(a.querySelector('span#soldCount').textContent)
-            const price1 = Number(b.querySelector('span#soldCount').textContent)
+            const price = Number(a.querySelector('p#soldCount').textContent)
+            const price1 = Number(b.querySelector('p#soldCount').textContent)
             return price1 - price;
         }).forEach(element => containerProducts.appendChild(element));
     }
