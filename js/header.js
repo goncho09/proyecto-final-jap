e3472eea20e7a028a2c9537bc1ef6ddff4a60619
goncho09 <gonzalolarricaV2@gmail.com>
2025-09-24 18:18:44 -0300

    refactorizacion header y cambio visual product info

--- js/header.js
+++ js/header.js
@@ -1,14 +1,20 @@
 function toggleMenu() {
-    const menu = document.getElementById('navbarSupportedContent');
-    menu.classList.toggle('active');
-  }
-  
-
-const cerrar = document.getElementById('cerrar-sesion');
-
-cerrar.addEventListener('click', cerrarSesion);
+  const menu = document.getElementById('navbarSupportedContent');
+  menu.classList.toggle('active');
+}
 
 function cerrarSesion() {
-    localStorage.removeItem("usuarioAutenticado");
-    window.location.replace('./login.html');
+  localStorage.removeItem('usuarioAutenticado');
+  window.location.replace('./login.html');
 }
+
+function loadHeader() {
+  fetch('header.html')
+    .then((res) => res.text())
+    .then((data) => {
+      const container = document.getElementById('header');
+      container.innerHTML = data;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loadHeader);
