cab7b73469b78a6eac3829d70c4fc75a4878e0f9
antonyespindola <laserantyu@gmail.com>
2025-10-29 14:56:32 -0300

    pequeño cambio en changetheme y pucharseitem

--- js/pucharseitem.js
+++ js/pucharseitem.js
@@ -4,7 +4,6 @@ const btcomprar = document.getElementById("buy-button"); // Obtenemos el boton
 
 btcomprar.addEventListener("click", () => {  // Ponemos un escuchador de evento de click 
   const producto = localStorage.getItem("productID") // Definimos una constante de productos y los obtenemos
-  localStorage.setItem("Compras", JSON.stringify(producto)); // Guardamos en el localstorage los productos obtenidos de dar click en comprar en un string
   window.location.replace('./cart.html'); // Redirigimos a la pagina del carrito
 }); 
 
