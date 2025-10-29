523a5562e11f5119b2b0daf88468ff02bd48e946
Donovan Acosta <dnvnacosta81@gmail.com>
2025-10-29 15:03:11 -0300

    agregando badge a cart

--- js/header.js
+++ js/header.js
@@ -1,34 +1,38 @@
 function toggleMenu() {
-  const menu = document.getElementById('navbarSupportedContent');
-  menu.classList.toggle('active');
+    const menu = document.getElementById('navbarSupportedContent');
+    menu.classList.toggle('active');
 }
 
 function cerrarSesion() {
-  localStorage.removeItem('usuarioAutenticado');
-  window.location.replace('./login.html');
+    localStorage.removeItem('usuarioAutenticado');
+    window.location.replace('./login.html');
 }
 
 function loadHeader() {
-  const container = document.getElementById('header');
+    const container = document.getElementById('header');
 
-  document.body.style.opacity = 0;
-  document.body.style.transition = 'opacity 0.2s ease';
+    document.body.style.opacity = 0;
+    document.body.style.transition = 'opacity 0.2s ease';
 
-  fetch('header.html')
-    .then((res) => res.text())
-    .then((data) => {
-      container.innerHTML = data;
+    fetch('header.html')
+        .then((res) => res.text())
+        .then((data) => {
+            container.innerHTML = data;
 
-      const userDisplay = document.getElementById('userDisplay');
-      const user = localStorage.getItem('usuarioAutenticado');
-      if (user && userDisplay) {
-        userDisplay.textContent = user;
-      }
-    })
-    .catch((err) => console.error('Error cargando el header:', err))
-    .finally(() => {
-      document.body.style.opacity = 1;
-    });
+            const userDisplay = document.getElementById('userDisplay');
+            const user = localStorage.getItem('usuarioAutenticado');
+            if (user && userDisplay) {
+                userDisplay.textContent = user;
+            }
+
+            const numberProducts = document.getElementById('number-products');
+
+            numberProducts.textContent = !localStorage.Compras ? 0 : JSON.parse(localStorage.Compras).length;
+        })
+        .catch((err) => console.error('Error cargando el header:', err))
+        .finally(() => {
+            document.body.style.opacity = 1;
+        });
 }
 
 document.addEventListener('DOMContentLoaded', loadHeader);
