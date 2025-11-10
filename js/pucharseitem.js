1076aa6266e06c45763cd9cae0a59a8bae4ed25a
Donovan Acosta <dnvnacosta81@gmail.com>
2025-11-10 16:41:51 -0300

    update cart

--- js/pucharseitem.js
+++ js/pucharseitem.js
@@ -1,25 +1,31 @@
 function guardarProductoCarrito() {
     // Definimos una funcion para ejecutarla al hacer click al boton de comprar
 
-    const btcomprar = document.getElementById('buy-button'); // Obtenemos el boton
-    btcomprar.addEventListener('click', () => {
-        // Ponemos un escuchador de evento de click
-        const producto = localStorage.getItem('productID'); // Definimos una constante de productos y los obtenemos
-        const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-        if (carrito.find((p) => parseInt(p.id) === parseInt(producto))) {
-            btcomprar.disabled = true;
-            btcomprar.textContent = 'Producto agregado al carrito';
+    const btnBuy = document.getElementById('buy-button'); // Obtenemos el boton
+    btnBuy.addEventListener('click', () => {
+
+        const cart = JSON.parse(localStorage.getItem('carrito')) || [];
+
+        let product = cart.find(product => product.id === localStorage.getItem('productID'))
+
+        if (product) {
+            product.cantidad++;
         } else {
-            carrito.push({
-                id: producto,
+            let [title, , price] = document.getElementById('product-details').children
+            let [first] = document.querySelectorAll('.thumbnail')
+
+            cart.push({
+                id: localStorage.productID,
+                title: title.textContent,
+                price: price.textContent,
+                image: first.src.split(3000)[1],
                 cantidad: 1,
             });
-            localStorage.setItem('carrito', JSON.stringify(carrito)); // Guardamos en el localstorage los productos obtenidos de dar click en comprar en un string
-            btcomprar.disabled = true;
-            btcomprar.textContent = 'Producto agregado al carrito';
-
         }
 
+        localStorage.setItem('carrito', JSON.stringify(cart)); // Guardamos en el localstorage los productos obtenidos de dar click en comprar en un string
+        btnBuy.disabled = true;
+        btnBuy.textContent = 'Producto agregado al carrito';
         window.location.replace('./cart.html')
     });
 }
